28b9ff21c7ccae299830724b0b2e04f5d8c78dde
NB <nb@FaStiCkeR.local>
2026-05-04 14:18:57 +0300

    fixes + operations

--- Lab4/Vector.h
+++ Lab4/Vector.h
@@ -3,19 +3,19 @@
 
 #include <iostream>
 #include <stdexcept>
-#include <string>   // для работы со строками
+#include <string>
 
-// Операторы для std::string, необходимые для работы Vector<std::string>
-// Определены до шаблона, чтобы быть видимыми при инстанцировании.
+// ------------------- операторы для строк -------------------
+// Вычитание: удаление ПЕРВОГО вхождения
 inline std::string operator-(const std::string &a, const std::string &b) {
     std::string result = a;
-    size_t pos = 0;
-    while ((pos = result.find(b, pos)) != std::string::npos) {
+    size_t pos = result.find(b);
+    if (pos != std::string::npos)
         result.erase(pos, b.length());
-    }
     return result;
 }
 
+// Умножение: повторение строки a столько раз, какова длина b
 inline std::string operator*(const std::string &a, const std::string &b) {
     std::string result;
     size_t repeat = b.length();
@@ -24,7 +24,7 @@ inline std::string operator*(const std::string &a, const std::string &b) {
     return result;
 }
 
-// Шаблонный класс Vector
+// ------------------- шаблонный класс Vector -------------------
 template<typename T>
 class Vector {
 private:
@@ -32,21 +32,14 @@ private:
     size_t size;
 
 public:
-    // Конструкторы
-    Vector() : Vector(0) {
-    }
-
-    explicit Vector(size_t n) : data(new T[n]()), size(n) {
-    }
-
+    Vector() : Vector(0) {}
+    explicit Vector(size_t n) : data(new T[n]()), size(n) {}
     Vector(const Vector &other) : data(new T[other.size]), size(other.size) {
         for (size_t i = 0; i < size; ++i)
             data[i] = other.data[i];
     }
-
     ~Vector() { delete[] data; }
 
-    // Оператор присваивания
     Vector &operator=(const Vector &other) {
         if (this != &other) {
             delete[] data;
@@ -58,12 +51,10 @@ public:
         return *this;
     }
 
-    // Доступ по индексу
     T &operator[](size_t index) {
         if (index >= size) throw std::out_of_range("Index out of range");
         return data[index];
     }
-
     const T &operator[](size_t index) const {
         if (index >= size) throw std::out_of_range("Index out of range");
         return data[index];
@@ -71,7 +62,7 @@ public:
 
     size_t getSize() const { return size; }
 
-    // Перегруженные операции
+    // Арифметические операции
     Vector operator+(const Vector &other) const {
         if (size != other.size) throw std::invalid_argument("Sizes differ");
         Vector result(size);
@@ -88,21 +79,46 @@ public:
         return result;
     }
 
-    // Скалярное произведение (для чисел) или соответствующая операция для строк
     T operator*(const Vector &other) const {
         if (size != other.size) throw std::invalid_argument("Sizes differ");
-        T sum = T(); // значение по умолчанию (0 для чисел, "" для строк)
+        T sum = T();
         for (size_t i = 0; i < size; ++i)
             sum = sum + data[i] * other.data[i];
         return sum;
     }
 
+    // Печать (можно удалить, оставив только operator<<)
     void print() const {
         std::cout << "[ ";
         for (size_t i = 0; i < size; ++i)
             std::cout << data[i] << " ";
         std::cout << "]" << std::endl;
     }
+
+    // Дружественные функции ввода/вывода
+    template<typename U>
+    friend std::ostream &operator<<(std::ostream &os, const Vector<U> &vec);
+
+    template<typename U>
+    friend std::istream &operator>>(std::istream &is, Vector<U> &vec);
 };
 
-#endif
+// ------------------- реализация ввода/вывода -------------------
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const Vector<T> &vec) {
+    os << "[ ";
+    for (size_t i = 0; i < vec.size; ++i)
+        os << vec.data[i] << " ";
+    os << "]";
+    return os;
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &is, Vector<T> &vec) {
+    for (size_t i = 0; i < vec.size; ++i) {
+        is >> vec.data[i];
+    }
+    return is;
+}
+
+#endif // VECTOR_H
--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,39 +1,28 @@
 #include "Vector.h"
-
+#include <iostream>
+#include <string>
 
 /**
  * Разработать шаблонный класс Вектор. Предусмотреть вариант хранения в векторе строк символов. Перегрузить операции
- * +, -. *. В классе должен быть конструктор. Предусмотреть член-функцию для
+ * +,-. *. В классе должен быть конструктор. Предусмотреть член-функцию для
  * печати элементов класса.
  */
+
 int main() {
-    // ----- Работа с целыми числами -----
+    // ---------- 1. Целые числа ----------
     Vector<int> a(3);
-    a[0] = 1;
-    a[1] = 2;
-    a[2] = 3;
+    a[0] = 1; a[1] = 2; a[2] = 3;
     Vector<int> b(3);
-    b[0] = 4;
-    b[1] = 5;
-    b[2] = 6;
+    b[0] = 4; b[1] = 5; b[2] = 6;
 
-    std::cout << "a = ";
-    a.print();
-    std::cout << "b = ";
-    b.print();
+    std::cout << "a = " << a << "\n";
+    std::cout << "b = " << b << "\n";
 
-    Vector<int> sum = a + b;
-    std::cout << "a + b = ";
-    sum.print();
+    std::cout << "a + b = " << a + b << "\n";
+    std::cout << "a - b = " << a - b << "\n";
+    std::cout << "a * b = " << a * b << "\n\n";
 
-    Vector<int> diff = a - b;
-    std::cout << "a - b = ";
-    diff.print();
-
-    int dot = a * b;
-    std::cout << "a * b (скалярное) = " << dot << std::endl;
-
-    // ----- Работа со строками -----
+    // ---------- 2. Строки ----------
     Vector<std::string> sa(2);
     sa[0] = "Hello";
     sa[1] = "World";
@@ -42,33 +31,29 @@ int main() {
     sb[0] = " ";
     sb[1] = "!";
 
-    std::cout << "\nsa = ";
-    sa.print();
-    std::cout << "sb = ";
-    sb.print();
+    std::cout << "sa = " << sa << "\n";
+    std::cout << "sb = " << sb << "\n";
+    std::cout << "sa + sb = " << sa + sb << "\n";   // конкатенация
+    std::cout << "sa * sb = " << sa * sb << "\n\n"; // повтор + сумма
 
-    Vector<std::string> s_sum = sa + sb; // конкатенация
-    std::cout << "sa + sb = ";
-    s_sum.print();
-
+    // Тест «удаление подстроки»
     Vector<std::string> sc(2);
     sc[0] = "HelloHello";
     sc[1] = "WorldWorld";
+
     Vector<std::string> sd(2);
     sd[0] = "Hello";
     sd[1] = "World";
 
-    std::cout << "sc = ";
-    sc.print();
-    std::cout << "sd = ";
-    sd.print();
+    std::cout << "sc = " << sc << "\n";
+    std::cout << "sd = " << sd << "\n";
+    std::cout << "sc - sd = " << sc - sd << "\n\n"; // удаление первого вхождения
 
-    Vector<std::string> s_diff = sc - sd; // удаление подстроки
-    std::cout << "sc - sd = ";
-    s_diff.print();
-
-    std::string s_dot = sa * sb; // повторение строки
-    std::cout << "sa * sb = " << s_dot << std::endl;
+    // ---------- 3. Ввод вектора ----------
+    std::cout << "Enter 3 integers: ";
+    Vector<int> userVec(3);
+    std::cin >> userVec;
+    std::cout << "You entered: " << userVec << "\n";
 
     return 0;
 }
